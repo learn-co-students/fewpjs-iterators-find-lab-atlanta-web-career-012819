ed388f40f1ce503f1a8b23c140a271c3bd31e455
wulman16 <will.ulman@gmail.com>
2019-03-20 10:11:55 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  const win = arr.find(record => record.result === 'W')
+  return (win ? win.year : win)
+}
